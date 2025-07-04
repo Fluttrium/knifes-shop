6279faf6f6ab50a49b078204e83a1404f8870979
Turbobot <turbobot@vercel.com>
2025-07-04 21:07:32 +0200

    feat(create-turbo): apply official-starter transform

--- packages/database/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { PrismaClient } from './generated';
-export declare const prisma: PrismaClient<import("./generated").Prisma.PrismaClientOptions, never, import("@repo/database/generated/runtime/library").DefaultArgs>;
-export * from './generated';
-export type { Prisma } from './generated';
--- packages/database/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from './generated'
-
-export const prisma = new PrismaClient()
-
-export * from './generated'
-export type { Prisma } from './generated'
